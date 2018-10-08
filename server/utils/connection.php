2b8b1f2136a3525b9590ecae7338f3569bb33d69
Marcos Antonio dos Santos <marcaosi2014@gmail.com>
2018-10-08 16:51:13 -0300

    initiallizing configuration project

---
+++ server/utils/connection.php
@@ -0,0 +1,49 @@
+<?php
+    $server = "localhost";
+    $db = "mammycare";
+    $user = "root";
+    $pwd = "";
+
+function getConnection(){
+    $conn = mysqli_connect($server, $user, $pwd, $db);
+    if(!$conn){
+        echo "Erro ao conectar ao banco de dados.";
+        die();
+    }
+
+    return $conn;
+}
+
+function closeConnection($conn){
+    mysqli_close($conn);
+}
+
+function query($sql, $conn){
+    if(strpos(strtoupper($sql), "SELECT")){
+        throw new Exception("Impossível executar query 'SELECT'. Use query_string()");
+    }
+
+    $result = mysqli_query($conn, $sql);
+
+    if($result){
+        return true;
+    }else{
+        throw new Exception("Impossível executar a query.");
+    }
+}
+
+function query_string($sql, $conn){
+    if(!strpos(strtoupper($sql), "SELECT")){
+        throw new Exception("Impossível executar query diferente de 'SELECT'. Use query()");
+    }
+
+    $result = mysqli_query($conn, $sql);
+
+    $array = array();
+
+    while($line = mysqli_fetch_assoc($result)){
+        array_push($line);
+    }
+
+    return $array;
+}
