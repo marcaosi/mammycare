86c939a3e1b9e7f31937852017c35edb7f48093d
Marcos Antonio dos Santos <MarcosAntonio@fuvs.br>
2019-07-05 15:12:43 -0300

    Updates

---
+++ precache-manifest.a24aff66bda4bd3765dbdd96932a1def.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "701a16bf28beb0487ea8",
+    "url": "/static/css/main.6b141387.chunk.css"
+  },
+  {
+    "revision": "701a16bf28beb0487ea8",
+    "url": "/static/js/main.701a16bf.chunk.js"
+  },
+  {
+    "revision": "5b849c07e2ce83e1323e",
+    "url": "/static/js/1.5b849c07.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "37314fd629afd354e427a2bdde791219",
+    "url": "/static/media/artificios_nao_recomendados.37314fd6.jpg"
+  },
+  {
+    "revision": "50686fad7c3631ae740f683d7b4db0cf",
+    "url": "/static/media/caixas_termicas.50686fad.jpg"
+  },
+  {
+    "revision": "32b564304eb8712d6e9ab3f82c86417f",
+    "url": "/static/media/copinho.32b56430.jpg"
+  },
+  {
+    "revision": "100abf1d8fe52a55bfbb0ffe88ae4c62",
+    "url": "/static/media/desenhos_diversos.100abf1d.jpg"
+  },
+  {
+    "revision": "da563a7bf920496aad1a4ca98f5bc941",
+    "url": "/static/media/desenhos_diversos2.da563a7b.jpg"
+  },
+  {
+    "revision": "5fc5539399060458f6623d5ce2d905ce",
+    "url": "/static/media/desenhos_diversos3.5fc55393.jpg"
+  },
+  {
+    "revision": "b7a9bc32e538542479eea7f251e3680e",
+    "url": "/static/media/equipamentos_amamentacao.b7a9bc32.jpg"
+  },
+  {
+    "revision": "5b1e516442d9a318d62464326d8c7612",
+    "url": "/static/media/equipamentos_amamentacao2.5b1e5164.jpg"
+  },
+  {
+    "revision": "8753ef9c816200199b54e8a6bcd2741d",
+    "url": "/static/media/estimulo_manual.8753ef9c.png"
+  },
+  {
+    "revision": "d289e3dbf50bea228c43f0e29cbcd6b6",
+    "url": "/static/media/final_da_mamada.d289e3db.jpg"
+  },
+  {
+    "revision": "7ab1361254ef2a324a47dfbf28dfa96f",
+    "url": "/static/media/formas_adequadas.7ab13612.jpg"
+  },
+  {
+    "revision": "47d5cd1d6eb32fc10d3fb530a3336e36",
+    "url": "/static/media/formas_armazenar.47d5cd1d.jpg"
+  },
+  {
+    "revision": "98184eef08d91bed9ae3f0125c6ab6a2",
+    "url": "/static/media/higienizacao_mamas.98184eef.jpg"
+  },
+  {
+    "revision": "0b3537e76ef6d24f08083d863efb2570",
+    "url": "/static/media/mamas.0b3537e7.png"
+  },
+  {
+    "revision": "9ebf0edf30117e9088c7e7827bff24c5",
+    "url": "/static/media/ordenha_manual.9ebf0edf.jpg"
+  },
+  {
+    "revision": "e1f9006108cdb982f09bb42aa3e69fb2",
+    "url": "/static/media/ordenha_manual2.e1f90061.jpg"
+  },
+  {
+    "revision": "ab4fddd68362ddc021d5395d3eab66cb",
+    "url": "/static/media/pega_correta.ab4fddd6.jpg"
+  },
+  {
+    "revision": "455d5461b2c4333c5414d9c32f126004",
+    "url": "/static/media/pega_correta2.455d5461.jpg"
+  },
+  {
+    "revision": "1ddf54fb2db356ebf229d633e248de76",
+    "url": "/static/media/pega_correta3.1ddf54fb.jpg"
+  },
+  {
+    "revision": "bd313622f66ff8e4a287467964cbec74",
+    "url": "/static/media/posicao_amamentar.bd313622.jpg"
+  },
+  {
+    "revision": "2eaea717df6e7773d09c28bdcdbe8206",
+    "url": "/static/media/posicao_amamentar2.2eaea717.jpg"
+  },
+  {
+    "revision": "23aadfaf16d7b6c0b54ce48377e2892a",
+    "url": "/static/media/posicao_amamentar3.23aadfaf.jpg"
+  },
+  {
+    "revision": "23be6c513e3a5d96f4e2bdb1fd5f993c",
+    "url": "/static/media/posicao_amamentar4.23be6c51.jpg"
+  },
+  {
+    "revision": "75d9953ac02f7f2744d03b131e63550b",
+    "url": "/static/media/posicao_amamentar5.75d9953a.jpg"
+  },
+  {
+    "revision": "58cab07efb1ded7dd96088e203994738",
+    "url": "/static/media/posicao_amamentar6.58cab07e.jpg"
+  },
+  {
+    "revision": "f29dbd68d512828161a97d2b50356730",
+    "url": "/static/media/tipos_mamilo.f29dbd68.png"
+  },
+  {
+    "revision": "808a04d43467a4bd95587d176d6e2a09",
+    "url": "/static/media/vesicula_avancada.808a04d4.jpg"
+  },
+  {
+    "revision": "cf86ffad9a7e4d231a636c0fd4ccce5f",
+    "url": "/static/media/reynaud.cf86ffad.png"
+  },
+  {
+    "revision": "8b2cac513be0786bee7dc177c66f2707",
+    "url": "/static/media/cand_mamilo_claro.8b2cac51.png"
+  },
+  {
+    "revision": "859f665b251a1b72c41d5d713c6fec5b",
+    "url": "/static/media/cand_mamilo_escuro.859f665b.png"
+  },
+  {
+    "revision": "5791662c8c55fbe6b4d144546355e218",
+    "url": "/static/media/fissura.5791662c.jpg"
+  },
+  {
+    "revision": "9f1d91a1805022ee33d6a09cac5daf84",
+    "url": "/static/media/fissura_protuso.9f1d91a1.png"
+  },
+  {
+    "revision": "b923e374c06c35e9f00acf2849e893f2",
+    "url": "/static/media/fissura_fotos.b923e374.png"
+  },
+  {
+    "revision": "02058d204e87623261df8c3883461260",
+    "url": "/static/media/escoriacao_erosa.02058d20.jpg"
+  },
+  {
+    "revision": "d296c3838610bdae7518500f9433fcd3",
+    "url": "/static/media/erosao_fotos.d296c383.png"
+  },
+  {
+    "revision": "bb9bb6d275af1fd3bbcecb88caf46b34",
+    "url": "/static/media/dilaceracao.bb9bb6d2.jpeg"
+  },
+  {
+    "revision": "19385a15a219f51add8084f28660affd",
+    "url": "/static/media/escoriacao_inicial.19385a15.jpeg"
+  },
+  {
+    "revision": "1cf0750819225f1a8dc86bcd1b45f86b",
+    "url": "/static/media/vesicula.1cf07508.png"
+  },
+  {
+    "revision": "808a04d43467a4bd95587d176d6e2a09",
+    "url": "/static/media/vesicula_avancada.808a04d4.jpeg"
+  },
+  {
+    "revision": "1ae8e6ec8b3fca33710b79f83e2f30f4",
+    "url": "/index.html"
+  }
+];
