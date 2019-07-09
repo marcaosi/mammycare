5506fa9b6831f8bb94a9207c79ab94605e22874d
Marcos Antônio dos Santos <marcaosi2014@gmail.com>
2019-07-09 19:02:09 -0300

    adicionar verificar logado

--- front/src/App.js
+++ front/src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import { browserHistory } from 'react-router'
 
 class App extends Component {
+
+  componentWillMount(){
+    if(!localStorage.getItem("jwt"))
+      browserHistory.push("/login")
+  }
   render() {
     return (
       <div>
