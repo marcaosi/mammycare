e8aab31f7f4c2d38de3997d828ba2a9e3592bfd4
Marcos <marcaosi2014@gmail.com>
2019-10-22 22:15:03 -0300

    adicionando logos ao rodapé

--- front/src/components/Footer/Footer.js
+++ front/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component{
         return (
             <footer className="container-fluid">
                 <div className="row bg-light-pink justify-content-md-center">
-                    <div className="col-xs-12 col-sm-8 credits">
+                    <div className="col-xs-12 col-sm-6 credits">
                         <h5>Conteúdo desenvolvido no Mestrado Profissional em Ciências aplicas à Saúde - Univás</h5>
                         <p>
                                 Aluna: Iaura Mônica Cunha da Silva Esteves (iauramonica@ig.com.br)<br/>
@@ -17,6 +17,14 @@ class Footer extends Component{
                                 Fotos: Vera Vinha<br/>
                                 Ilustrações: Felipe Silva Lemes
                         </p>
+                    </div>
+                    <div className="col-sm-2">
+                        <a target="_blank" href="https://www.univas.edu.br/mpcas">
+                            <img src={require("../../img/mpcas.png")}/>
+                        </a>
+                        <a target="_blank" href="http://www.cnpq.br/">
+                            <img src={require("../../img/cnpq.png")}/>
+                        </a>
                     </div>
                 </div>
                 <div className="row bg-light-blue">
