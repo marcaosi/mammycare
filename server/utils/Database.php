1152cec226ef29f09f7a4af954ad4da48f48b08d
Marcos Antonio dos Santos <marcosantonio.dev@gmail.com>
2020-07-29 20:07:11 -0300

    changes

--- server/utils/Database.php
+++ server/utils/Database.php
@@ -1,7 +1,7 @@
 <?php
 
 class Database extends mysqli {
-    public function __construct($host = "mysql.hostinger.com.br", $user = "u673376319_mammy", $pass = "jSA3yRfoo0LF", $db = "u673376319_mammy"){
+    public function __construct($host = "localhost", $user = "progmn42_mammycare", $pass = "M@mmyc@re.", $db = "progmn42_mammycare"){
         parent::__construct($host, $user, $pass, $db);
 
         if(mysqli_connect_error()) throw new Exception("Erro ao conectar no banco de dados.");
